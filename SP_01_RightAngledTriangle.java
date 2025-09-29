2373781061349b92312d21db5e9170ffba0fc9f2
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-09-29 16:51:50 -0400

    Right-Angled Triangle pattern

---
+++ SP_01_RightAngledTriangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SP_01_RightAngledTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of lines: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("\nRight-Angled Triangle Pattern:\n");
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
