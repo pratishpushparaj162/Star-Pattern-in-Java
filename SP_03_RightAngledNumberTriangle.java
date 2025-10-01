159460c0ee2f4c6d97f133f20988fe5cce4edac9
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-10-01 14:01:20 -0400

    Right Angled Triangle Number-pattern

---
+++ SP_03_RightAngledNumberTriangle.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SP_03_RightAngledNumberTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of lines: ");
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }   
+}
+
+
+/*
+
+For n = 5 -->
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
