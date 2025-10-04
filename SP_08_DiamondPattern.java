953fca79f73775b3faa5f3344e0fa0e3882cf7fe
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-10-04 12:58:30 -0400

    Diamond Pattern

---
+++ SP_08_DiamondPattern.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class SP_08_DiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of lines : ");
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+       // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+/*
+
+For n = 6 -->
+
+         * 
+        * * * 
+      * * * * * 
+    * * * * * * * 
+  * * * * * * * * * 
+* * * * * * * * * * * 
+  * * * * * * * * * 
+    * * * * * * * 
+      * * * * * 
+        * * * 
+          * 
+*/
