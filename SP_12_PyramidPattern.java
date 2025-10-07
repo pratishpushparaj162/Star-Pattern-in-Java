3b6b3dfd6bb080597bcfd0f3a073df7fc142326a
Pranav Nikam <pranavnikam15pn@gmail.com>
2025-10-07 00:00:12 -0400

    Pyramid Pattern

---
+++ SP_12_PyramidPattern.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class SP_12_PyramidPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of lines : ");
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println();
+
+        for(int i=1; i<=n; i++){
+
+            // initial spaces
+            for(int k=i; k<n; k++){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int k=1; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+/*
+ 
+For n = 7 --> 
+
+       * 
+      * * 
+     * * * 
+    * * * * 
+   * * * * * 
+  * * * * * * 
+ * * * * * * * 
+* * * * * * * * 
+
+*/
